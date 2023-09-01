82d7e9c85c9b1f2cd6691c5b85b4b82123386548
dinukapeter <dinukapeter@gmail.com>
2023-09-01 21:39:16 +0530

    Update

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -4,23 +4,23 @@ import sys
 # Add the parent directory (project root) to the Python path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-import calculator
+from calculator import *
 
 def test_add():
-    assert calculator.add(2, 3) == 5
-    assert calculator.add(-1, 1) == 0
-    assert calculator.add(0, 0) == 0
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+    assert add(0, 0) == 0
 
 def test_subtract():
-    assert calculator.subtract(5, 3) == 2
-    assert calculator.subtract(0, 0) == 0
-    assert calculator.subtract(-1, -1) == 0
+    assert subtract(5, 3) == 2
+    assert subtract(0, 0) == 0
+    assert subtract(-1, -1) == 0
 
 def test_multiply():
-    assert calculator.multiply(2, 3) == 6
-    assert calculator.multiply(-1, 5) == -5
-    assert calculator.multiply(0, 100) == 0
+    assert multiply(2, 3) == 6
+    assert multiply(-1, 5) == -5
+    assert multiply(0, 100) == 0
 
 def test_divide():
-    assert calculator.divide(6, 2) == 3
-    assert calculator.divide(10, 2) == 5
+    assert divide(6, 2) == 3
+    assert divide(10, 2) == 5
